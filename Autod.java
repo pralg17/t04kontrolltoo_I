4b42eaaa81fcb7bc39832a553a446bed55883293
elleivan <elleivan@greeny.cs.tlu.ee>
2017-02-21 14:25:41 +0200

    Kontrolltoo teine punkt

--- Autod.java
+++ Autod.java
@@ -1,12 +1,28 @@
 public class Autod{
-  double a, b;
+  double a, b, c, d, e;
 
-  public Autod(double kaugus, double kiirus){
+  public Autod(double kaugus, double kiirus, double sekundid, double minutid, double tunnid){
     a = kaugus;
     b = kiirus;
+    c = sekundid;
+    d = minutid;
+    e = tunnid;
   }
+
+  public double sekundid(){
+    return b*c;
+  }
+
+  public double minutid(){
+    return b*d;
+  }
+
+  public double tunnid(){
+    return b*e;
+  }
+
   public String toString(){
-    return "Auto kaugus on "+a+" km ja kiirus on "+b+" km/h.";
+    return "Auto kaugus maantee algusest on "+a+" km ja kiirus on "+b+" km/h. Auto asukoht 3 sekundi pärast on "+sekundid()+" km, 5 minuti pärast "+minutid()+" km, ja 2 tunni pärast "+tunnid()+" km.";
   }
 
 }
