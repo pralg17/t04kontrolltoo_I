e7b1035cb90ac2d0057d308fc2655122906a4568
anusada <anusada@greeny.cs.tlu.ee>
2017-02-21 15:37:52 +0200

    correction

--- hulknurk.java
+++ hulknurk.java
@@ -1,5 +1,5 @@
 public class hulknurk{		
-	private static void printArray(int[] anArray) {
+	public static void printArray(int[] anArray) {
 		for (int i = 0; i < anArray.length; i++) {
 			if (i > 0) {
 				System.out.print(", ");
