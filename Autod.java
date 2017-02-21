c0ee0bd2608f05a89feb82c418a144de1bf0bd01
elleivan <elleivan@greeny.cs.tlu.ee>
2017-02-21 13:39:30 +0200

    Kontrolltoo esimene punkt

---
+++ Autod.java
@@ -0,0 +1,12 @@
+public class Autod{
+  double a, b;
+
+  public Autod(double kaugus, double kiirus){
+    a = kaugus;
+    b = kiirus;
+  }
+  public String toString(){
+    return "Auto kaugus on "+a+" km ja kiirus on "+b+" km/h.";
+  }
+
+}
---
+++ andmed.java
@@ -0,0 +1,8 @@
+public class andmed{
+  public static void main(String[] args){
+    Autod auto1=new Autod(50, 50);
+    Autod auto2=new Autod(75, 60);
+    System.out.println(auto1.toString());
+    System.out.println(auto2.toString());
+    }
+}
