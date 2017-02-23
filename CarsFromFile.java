c4f2a4a7ba516c64bb46b2f94a2a7950397ee444
siimhuts <siimhuts@greeny.cs.tlu.ee>
2017-02-23 09:35:56 +0200

    Started working on part 3

---
+++ CarsFromFile.java
@@ -0,0 +1,8 @@
+import java.io.*;
+public class CarsFormFile{
+    public static void main(String[] args) throws IOException{
+        BufferedReader lugeja= new BufferedReader(
+        new FileReader("Data.txt")
+        );        
+    }
+}
