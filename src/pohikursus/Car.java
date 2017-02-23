60de355c9e3464678b274e75a0289bcc6d3da5cf
alaraasa <alar@alaraasa.ee>
2017-02-23 08:55:10 +0200

    Punkt 2 valmis

--- src/pohikursus/Car.java
+++ src/pohikursus/Car.java
@@ -4,25 +4,24 @@ package pohikursus;
  * Created by AASA on 23.02.2017.
  */
 public class Car {
-    private int car1DistanceFromStart, car2DistanceFromStart;
-    private double car1Speed, car2Speed;
-    public Car(int car1DistanceFromStart, int car2DistanceFromStart, double car1Speed, double car2Speed){
+    /*KILOMETERS*/
+    private double car1DistanceFromStart, car2DistanceFromStart, car1Speed, car2Speed;
+    public Car(double car1DistanceFromStart, double car2DistanceFromStart, double car1Speed, double car2Speed){
         this.car1DistanceFromStart = car1DistanceFromStart;
         this.car2DistanceFromStart = car2DistanceFromStart;
         this.car1Speed = car1Speed;
         this.car2Speed = car2Speed;
     }
-
-    public int getCar1DistanceFromStart() {
+    public double getCar1DistanceFromStart() {
         return car1DistanceFromStart;
     }
-    public void setCar1DistanceFromStart(int car1DistanceFromStart) {
+    public void setCar1DistanceFromStart(double car1DistanceFromStart) {
         this.car1DistanceFromStart = car1DistanceFromStart;
     }
-    public int getCar2DistanceFromStart() {
+    public double getCar2DistanceFromStart() {
         return car2DistanceFromStart;
     }
-    public void setCar2DistanceFromStart(int car2DistanceFromStart) {
+    public void setCar2DistanceFromStart(double car2DistanceFromStart) {
         this.car2DistanceFromStart = car2DistanceFromStart;
     }
     public double getCar1Speed() {
@@ -38,4 +37,10 @@ public class Car {
         this.car2Speed = car2Speed;
     }
 
+    public void newDistance(int seconds){
+        double car1Speed = (getCar1Speed()/60)/60;
+        double car2Speed = (getCar2Speed()/60)/60;
+        setCar1DistanceFromStart(getCar1DistanceFromStart()+(car1Speed*seconds));
+        setCar2DistanceFromStart(getCar2DistanceFromStart()+(car2Speed*seconds));
+    }
 }
--- src/pohikursus/Main.java
+++ src/pohikursus/Main.java
@@ -1,20 +1,38 @@
 package pohikursus;
 
 public class Main {
-
+/*
+AUTOD TEEL
+ */
     public static void main(String[] args) {
         Car cars1 = new Car(10, 15, 15, 10);
-        Car cars2 = new Car(100, 0, 0, 100);
         System.out.println("Cars1 Car 1 distance: " + cars1.getCar1DistanceFromStart());
+        System.out.println("Cars1 Car 1 speed: " + cars1.getCar1Speed());
+        System.out.println("Cars1 Car 2 distance: " + cars1.getCar2DistanceFromStart());
         System.out.println("Cars1 Car 2 speed: " + cars1.getCar2Speed());
-        System.out.println("Cars2 Car 1 speed: " + cars2.getCar1Speed());
-        System.out.println("Cars2 Car 1 distance: " + cars2.getCar2DistanceFromStart());
-
+        /* +3 SECONDS*/
+        cars1.newDistance(3);
+        System.out.println("Cars1 Car 1 new distance (3 sec): " + cars1.getCar1DistanceFromStart());
+        System.out.println("Cars1 Car 2 new distance (3 sec): " + cars1.getCar2DistanceFromStart());
+        /* +5 MINUTES / 300 SECONDS */
+        cars1.newDistance(300);
+        System.out.println("Cars1 Car 1 new distance (5 min): " + cars1.getCar1DistanceFromStart());
+        System.out.println("Cars1 Car 2 new distance (5 min): " + cars1.getCar2DistanceFromStart());
+        /* +2 HOURS / */
+        cars1.newDistance(7200);
+        System.out.println("Cars1 Car 1 new distance (2 h): " + cars1.getCar1DistanceFromStart());
+        System.out.println("Cars1 Car 2 new distance (2 h): " + cars1.getCar2DistanceFromStart());
         /*
-        Cars1 Car 1 distance: 10
+        Cars1 Car 1 distance: 10.0
+        Cars1 Car 1 speed: 15.0
+        Cars1 Car 2 distance: 15.0
         Cars1 Car 2 speed: 10.0
-        Cars2 Car 1 speed: 0.0
-        Cars2 Car 1 distance: 0
+        Cars1 Car 1 new distance (3 sec): 10.0125
+        Cars1 Car 2 new distance (3 sec): 15.008333333333333
+        Cars1 Car 1 new distance (5 min): 11.2625
+        Cars1 Car 2 new distance (5 min): 15.841666666666667
+        Cars1 Car 1 new distance (2 h): 41.2625
+        Cars1 Car 2 new distance (2 h): 35.84166666666666
          */
     }
 }
