659ee544ca310115190cdb262050a445b727e216
Rihardryys <Rihardryys@users.noreply.github.com>
2017-02-23 08:29:03 +0200

    harmooniline keskmine punkt1

---
+++ proov1.java
@@ -0,0 +1,15 @@
+public class proov1{
+	public static void main(String[] args){
+		
+		harmooniline ajad1=new harmooniline(20, 40);
+		harmooniline ajad2=new harmooniline(9, 35);
+		
+		System.out.println(ajad1.kiirused1()+" km/h");
+		System.out.println(ajad1.kiirused2()+" km/h");
+		System.out.println(ajad2.kiirused1()+" km/h");
+		System.out.println(ajad2.kiirused2()+" km/h");
+		
+
+		
+	}
+}
