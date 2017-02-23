2572ee7e38f3b6301ab9c8dc97ca738a61635dda
siimhuts <siimhuts@greeny.cs.tlu.ee>
2017-02-23 09:15:35 +0200

    Fixed wrong data file

--- Data.java
+++ Data.java
@@ -1,9 +1,9 @@
 public class Data{
    public static void main(String[] args){
-     Cars car1=new Cars(350, 60, 15, 35, 2);
-     Cars car2=new Cars(120, 55, 10, 20, 1);
-     Cars car3=new Cars(445, 100, 35, 40, 4);
-     Cars car4=new Cars(75, 30, 40, 40, 1);
+     Cars car1=new Cars(350, 60, 3, 5, 2);
+     Cars car2=new Cars(120, 55, 3, 5, 2);
+     Cars car3=new Cars(445, 100, 3, 5, 2);
+     Cars car4=new Cars(75, 30, 3, 5, 2);
      System.out.println(car1.toString());
      System.out.println(car2.toString());
      System.out.println(car3.toString());
