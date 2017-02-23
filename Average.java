da95d41353c89dac58ec55b000b2d9b7e4bde49c
Rando Tomingas <randotm@users.noreply.github.com>
2017-02-23 08:30:04 +0200

    Finished first excercise.

---
+++ Average.java
@@ -0,0 +1,16 @@
+public class Average{
+
+	public static float averageOfThree(int number1, int number2, int number3) {
+		return (number1 + number2 + number3) / 3;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(averageOfThree(3,4,5));
+	}
+
+}
+
+/*
+java Average
+4.0
+*/
