5304cc6da67f65107086554a97b788cad37586a2
Hinrek Saar <hinrek@gmail.com>
2017-02-23 08:52:20 +0200

    2. Valmis

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,28 @@
 public class Main {
 
     public static void main(String[] args) {
+        int[] numbers = {4, 7, 2};
+
         System.out.println(multiplySqrt(10, 20));
+        System.out.println(multiplyPowArray(numbers));
     }
 
-     static double multiplySqrt(double x, double y){
+    private static double multiplySqrt(double x, double y) {
         double i;
         i = (x * y);
         i = Math.sqrt(i);
         return i;
     }
+
+    private static double multiplyPowArray(int[] array) {
+        int counter = 0;
+        double multiplyPow = 1;
+        for (int i : array) {
+            counter++;
+            multiplyPow = i * multiplyPow;
+        }
+        return Math.pow(multiplyPow, counter);
+    }
+
+
 }
