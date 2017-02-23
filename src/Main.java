7f31ef76ade57ae5764eab23082ada34841b0568
Hinrek Saar <hinrek@gmail.com>
2017-02-23 17:08:24 +0200

    3. 75% Valmis

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,35 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
 
+    private static ArrayList<Double> newSalarys = new ArrayList<>();
+
     public static void main(String[] args) throws IOException {
         int[] numbers = {4, 7, 2};
-        String firstYearSalaryString = readFile("palgad.txt").get(0);
-        Double firsYearSalary = Double.parseDouble(firstYearSalaryString);
+        String inputFilename = "palgad.txt";
+        double firstMonthSalary = readFile(inputFilename).get(0);
+        Double salarySum = 0.0;
 
+        List<Double> salaryChange = readFile(inputFilename).subList(1, (readFile(inputFilename).size()));
 
         System.out.println(multiplySqrt(10, 20));
         System.out.println(multiplyPowArray(numbers));
+        
+        newSalarys.add(firstMonthSalary);
+        for (Double i : salaryChange) {
+            newSalarys.add(calculateNewSalary(firstMonthSalary, i));
+        }
 
-        System.out.println(readFile("palgad.txt"));
-        System.out.println();
+        String outputFilename = "uuedpalgad.txt";
+        createFile(outputFilename, newSalarys);
 
-        double[] newSalarys = new double[10];
-        for (String i : readFile("palgad.txt")) {
-
-            double n = Double.parseDouble(i);
-            double s = firsYearSalary * n;
-            System.out.println(s);
-
+        for (Double i : newSalarys) {
+            salarySum = salarySum + i;
         }
     }
 
@@ -43,24 +50,25 @@ public class Main {
         return Math.pow(multiplyPow, counter);
     }
 
-    private static List<String> readFile(String filename) throws IOException {
+    private static ArrayList<Double> readFile(String filename) throws IOException {
         BufferedReader reader = new BufferedReader(new FileReader(filename));
-        List<String> salary = new ArrayList<>();
+        ArrayList<Double> salary = new ArrayList<>();
         String s;
         while ((s = reader.readLine()) != null) {
-            salary.add(s);
+            salary.add(Double.parseDouble(s));
         }
         return salary;
     }
 
-    private static void createFile(String filename, String content) {
-        try (Writer writer = new BufferedWriter(new OutputStreamWriter(
-                new FileOutputStream(filename), "utf-8"
-        ))) {
-            writer.write(content);
+    private static Double calculateNewSalary(Double salary, Double salaryChange) {
+        return salaryChange * salary;
+    }
+
+    private static void createFile(String file, ArrayList<Double> arrData)
+            throws IOException {
+        try (FileWriter writer = new FileWriter(file + ".txt")) {
+            for (Double anArrData : arrData) writer.write(anArrData.toString() + "\n");
             writer.close();
-        } catch (IOException ex) {
-            System.out.println("IOException");
         }
     }
 }
