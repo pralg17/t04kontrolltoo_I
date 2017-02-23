3c54adba1bf5aa8a9a924f2675e8ec7b4c172a14
karlbork <karlbork@greeny.cs.tlu.ee>
2017-02-23 09:43:24 +0200

    added second part

--- KT.java
+++ KT.java
@@ -1,8 +1,12 @@
+//import java.util.Arrays;
+
 public class KT {
 	public static void main(String[] args) {
 		
 		double numbers1 = average(3, 4, 5);
+		double[] numbers2 = new double[] {23,5,67,43,201,22,9,70,78,100};
 		System.out.println("Arvude kronoloogiline keskmine on " + numbers1);
+		System.out.println(averageArray(numbers2));
 		
 	}
 	
@@ -12,4 +16,24 @@ public class KT {
 		return average;
 		
 	}
+	
+	public static double averageArray(double[] numbers2) {
+		
+		double averageFirstNum = numbers2[0] / 2;
+		double averageLastNum = numbers2[numbers2.length-1] / 2;
+		double averageMiddleNums = 0;
+		int arrayLength = numbers2.length;
+		
+		
+		for(int i = 1; i < numbers2.length-1; i++) {
+			averageMiddleNums = averageMiddleNums + numbers2[i];
+		}
+		
+		double average = (averageFirstNum + averageMiddleNums + averageLastNum) / (numbers2.length-1);
+		
+		return average;
+		// 11.5 + 495 + 50 = 556.5
+		//556.5 / 9.0
+	}
+	
 }
