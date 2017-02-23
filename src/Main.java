018aaea5cb51ad76695bd5b8e247379188d7e5d5
Hinrek Saar <hinrek@gmail.com>
2017-02-23 08:28:01 +0200

    1. Valmis

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(multiplySqrt(10, 20));
+    }
+
+     static double multiplySqrt(double x, double y){
+        double i;
+        i = (x * y);
+        i = Math.sqrt(i);
+        return i;
+    }
+}
