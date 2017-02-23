ae9d3775bc1ae4fc629f1d64b82f077ae115d813
Hinrek Saar <hinrek@gmail.com>
2017-02-23 09:41:02 +0200

    3. Poolik

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,29 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         int[] numbers = {4, 7, 2};
+        String firstYearSalaryString = readFile("palgad.txt").get(0);
+        Double firsYearSalary = Double.parseDouble(firstYearSalaryString);
+
 
         System.out.println(multiplySqrt(10, 20));
         System.out.println(multiplyPowArray(numbers));
+
+        System.out.println(readFile("palgad.txt"));
+        System.out.println();
+
+        double[] newSalarys = new double[10];
+        for (String i : readFile("palgad.txt")) {
+
+            double n = Double.parseDouble(i);
+            double s = firsYearSalary * n;
+            System.out.println(s);
+
+        }
     }
 
     private static double multiplySqrt(double x, double y) {
@@ -25,5 +43,24 @@ public class Main {
         return Math.pow(multiplyPow, counter);
     }
 
+    private static List<String> readFile(String filename) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(filename));
+        List<String> salary = new ArrayList<>();
+        String s;
+        while ((s = reader.readLine()) != null) {
+            salary.add(s);
+        }
+        return salary;
+    }
 
+    private static void createFile(String filename, String content) {
+        try (Writer writer = new BufferedWriter(new OutputStreamWriter(
+                new FileOutputStream(filename), "utf-8"
+        ))) {
+            writer.write(content);
+            writer.close();
+        } catch (IOException ex) {
+            System.out.println("IOException");
+        }
+    }
 }
