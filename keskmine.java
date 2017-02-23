b6297bb540b6348de0b84f9ce9540f9023afb6e2
johalaas <johalaas@greeny.cs.tlu.ee>
2017-02-23 09:55:41 +0200

    poolik esimene

---
+++ keskmine.java
@@ -0,0 +1,10 @@
+public class keskmine{
+	public static void main(Strings[] args){
+		double numbrid=keskmine(1,1,1);
+		System.out.println(numbrid);
+	}
+	public static double keskmine(double arv1, double arv2, double arv3){
+		keskmine = (arv1/2)+arv2+(arv3/2);
+		return keskmine;
+	}
+}
