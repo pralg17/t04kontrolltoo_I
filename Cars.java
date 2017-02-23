44b36641234797e1d2a7837f0b6a42ff45098439
siimhuts <siimhuts@greeny.cs.tlu.ee>
2017-02-23 09:39:21 +0200

    Added result for 1 and 2 part to Cars.java

--- Cars.java
+++ Cars.java
@@ -26,3 +26,11 @@ public String toString(){
     return "Auto kaugus on "+s+" km ja autode kiirus on "+v+" km/h auto, auto kaugus algusest 3 sekundi p2rast on "+sekund()+" kilomeetrit ning 5 minuti p2rast on kaugus "+minut()+" kilomeetrit ja 2 tunni p2rast "+tund()+" kilomeetrit";
     }
 }
+
+/* [siimhuts@greeny t04kontrolltoo_I]$ java Data
+Auto kaugus on 350.0 km ja autode kiirus on 60.0 km/h auto, auto kaugus algusest 3 sekundi p2rast on 350.25 kilomeetrit ning 5 minuti p2rast on kaugus 385.0 kilomeetrit ja 2 tunni p2rast 470.0 kilomeetrit
+Auto kaugus on 120.0 km ja autode kiirus on 55.0 km/h auto, auto kaugus algusest 3 sekundi p2rast on 120.15277777777777 kilomeetrit ning 5 minuti p2rast on kaugus 138.33333333333334 kilomeetrit ja 2 tunni p2rast 175.0 kilomeetrit
+Auto kaugus on 445.0 km ja autode kiirus on 100.0 km/h auto, auto kaugus algusest 3 sekundi p2rast on 445.97222222222223 kilomeetrit ning 5 minuti p2rast on kaugus 511.66666666666663 kilomeetrit ja 2 tunni p2rast 845.0 kilomeetrit
+Auto kaugus on 75.0 km ja autode kiirus on 30.0 km/h auto, auto kaugus algusest 3 sekundi p2rast on 75.33333333333333 kilomeetrit ning 5 minuti p2rast on kaugus 95.0 kilomeetrit ja 2 tunni p2rast 105.0 kilomeetrit
+[siimhuts@greeny t04kontrolltoo_I]$
+*/
