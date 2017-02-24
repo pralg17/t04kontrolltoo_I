0a2e455bcc8b72dd44820369570ae6f0a94d6278
vlasn <vlasn@tlu.ee>
2017-02-24 17:27:57 +0200

    Lahendasin ülesande lõpuni

--- Kolmnurk.java
+++ Kolmnurk.java
@@ -1,16 +1,18 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Kolmnurk {
 
-    Integer[] X;
-    Integer[] Y;
+    Double[] X;
+    Double[] Y;
     //Double[] sides;
     Integer sideCount;
     List<Double> sides = new ArrayList<>();
+    List<Double> distances = new ArrayList<>();
     Double[] midpoint;
 
-    public Kolmnurk(Integer[] newX, Integer[] newY) {
+    public Kolmnurk(Double[] newX, Double[] newY) {
         if (newX != null) {
             this.X = newX;
         } else {
@@ -22,20 +24,27 @@ public class Kolmnurk {
             throw new RuntimeException("No Y-coordinates");
         }
         sideCount = X.length;
-        //Calculating lengths of sides
+
         if(newX.length != newY.length){
             throw new RuntimeException("Need ei ole need droidid keda te otsite.");
         } else {
+
+            //Calculate midpoint
+            this.midpoint = calcMidpoint(X,Y);
+
+            //Calculating lengths of sides
             for(int i=1;i<X.length;i++) {
                 sides.add(calculateSide(X[i-1],Y[i-1],X[i],Y[i]));
             }
             sides.add(calculateSide(X[sideCount-1],Y[sideCount-1],X[0],Y[0]));
+
+            //Calculating points' distance from the center
+            for(int i=0;i<X.length;i++) {
+                distances.add(calculateSide(X[i],Y[i],midpoint[0],midpoint[1]));
+            }
         }
-        //Calculating midpoint
-        this.midpoint = calcMidpoint(X,Y);
-
     }
-
+    //Hälbeta võrdkülgsuse kontroll.
     public boolean equiLateral() {
         boolean isEquiLateral = true;
         for(int i=1;i<X.length;i++){
@@ -45,23 +54,29 @@ public class Kolmnurk {
         }
         return isEquiLateral;
     }
-
-    public Double calculateSide(int X1, int Y1,int X2, int Y2){
+    //Kahe koordinaadi vahelise kauguse arvutamine
+    public Double calculateSide(Double X1, Double Y1, Double X2, Double Y2){
         return(Math.sqrt(
-                (double)Math.pow((X2-X1),2)+
-                (double)Math.pow((Y2-Y1),2)
+                Math.pow((X2-X1),2) +
+                Math.pow((Y2-Y1),2)
         ));
     }
+    //Külgede printimiseks
     public List<Double> getSides(){
         return sides;
     }
 
-
+    //Võrdkülgsuse ja hälbe wrapper
+    public String eqLatDev(Integer margin){
+        return Boolean.toString(checkDistMargin(sides,margin));
+    }
+    //Keskpunkti stringi kujul esitamiseks
     public String getMidpoint() {
-        return String.valueOf(midpoint[0])+String.valueOf(midpoint[1]);
+        return Arrays.toString(midpoint);
     }
 
-    public Double[] calcMidpoint(Integer[] x,Integer[] y) {
+    //Meetod arvutamaks keskpunkti.
+    public Double[] calcMidpoint(Double[] x,Double[] y) {
         int xSum = 0;
         int ySum = 0;
         for(int i=0;i<x.length;i++) {
@@ -69,10 +84,28 @@ public class Kolmnurk {
             ySum += Y[i];
         }
 
-        Double xMid = (double)(xSum)/x.length;
-        Double yMid = (double)(ySum)/x.length;
+        Double xMid = (xSum)/(double)x.length;
+        Double yMid = (ySum)/(double)x.length;
         Double[] result = {xMid,yMid};
         return result;
     }
 
+    public Boolean cenDistDev(Integer margin){
+        return checkDistMargin(this.distances, margin);
+    }
+    //Listi liikmete keskmise arvutamine ja hälbe kontroll
+    public Boolean checkDistMargin(List<Double> input, Integer margin) {
+        boolean meetsCriteria = true;
+        double inputum = 0.0;
+        for(int i=0;i<input.size();i++) {
+            inputum += input.get(i);
+        }
+        Double sideAvg = inputum / input.size();
+        for(int i=0;i<input.size();i++){
+            if ((Math.abs(sideAvg - input.get(i)) / sideAvg) * 100.0 > (double)margin) {
+                meetsCriteria = false;
+            }
+        }
+        return(meetsCriteria);
+    }
 }
--- Wrapper.java
+++ Wrapper.java
@@ -1,11 +1,27 @@
 public class Wrapper{
     public static void main(String[] args){
 
-        Integer[] X = new Integer[] {1,2,3};
-        Integer[] Y = new Integer[] {3,1,2};
+        Double[] X = new Double[] {1.0,2.0,3.0};
+        Double[] Y = new Double[] {3.0,1.0,2.0};
+        Double[] X1 = new Double[] {1.4,2.3,0.3,-3.5};
+        Double[] Y1 = new Double[] {4.0,1.3,5.7,-2.5};
         Kolmnurk k1 = new Kolmnurk(X,Y);
+
+        System.out.println("Külgede pikkused:");
         System.out.println(k1.getSides());
+        System.out.println("Keskpunkti koordinaadid:");
         System.out.println(k1.getMidpoint());
+        System.out.println("Kontrollime, kas võrdkülgsus jääb järgmise protsendi piirdeisse:");
+        System.out.println(k1.eqLatDev(50));
+        System.out.println("Nüüd vaatame, kas punktide kaugus keskpaigast jääb järgmise protsendi ulatusse:");
+        System.out.println(k1.cenDistDev(10));
+
+        Kolmnurk hulknurk = new Kolmnurk(X1,Y1);
+        System.out.println("Samad näitajad hulknurga puhul:");
+        System.out.println(hulknurk.getSides());
+        System.out.println(hulknurk.getMidpoint());
+        System.out.println(hulknurk.eqLatDev(50));
+        System.out.println(hulknurk.cenDistDev(10));
 
     }
 }
