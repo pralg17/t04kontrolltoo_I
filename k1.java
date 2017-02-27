2b582a9cc752eb510714095bcc7fa48b072df635
edgaseli <edgaseli@greeny.cs.tlu.ee>
2017-02-27 12:56:14 +0200

    ulesanne 1, parandatud

--- k1.java
+++ k1.java
@@ -1,20 +1,27 @@
-import java.io.*;
-import java.util.*;
+import java.util.Scanner;
+
 public class k1 {
-	static double k1(double aine1, double aine2) {
-		return (aine1+aine2)/2;
-}	
-	static double hinned(int hinne1, int hinne2){
-		return (hinne1+hinne2)/2;
+	public static double weightedAvg(double h1, double h2, double a1, double a2) {
+		double avg = (h1*a1 + h2*a2) /(a1+a2);
+		return avg;
+		
+		
 	}
 	
 	public static void main (String[] args) {
-		System.out.println("Keskmine ainepunkt on: " + k1(70,54));
-		System.out.println("Keskmine hinne on: " + hinned(3,2));
-  }
+		int mark1, mark2, eap1, eap2;
+		
+		System.out.println("Enter marks: ");
+		Scanner in = new Scanner (System.in);
+		mark1 = in.nextInt();
+		mark2 = in.nextInt();
+		
+		System.out.println("Enter EAP:");
+		
+		eap1 = in.nextInt();
+		eap2 = in.nextInt();
+		
+		System.out.println("Weighted average is: " + weightedAvg(mark1,mark2,eap1,eap2));
+	}
+	
 }
-
-/* [edgaseli@greeny t04kontrolltoo_I]$ java k1
-Keskmine ainepunkt on: 62.0
-Keskmine hinne on: 2.0
-*/
