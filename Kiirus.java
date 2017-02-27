cb6333bdbb15c151d2a2fd3cf69df1a24b6f7439
Marko Kollo <mrxkollo@users.noreply.github.com>
2017-02-27 14:03:18 +0200

    Lõpptöö

--- Kiirus.java
+++ Kiirus.java
@@ -11,48 +11,72 @@
 /* psvm - main
  * sout- println */
 
-import java.io.IOException;
-import java.net.MalformedURLException;
+import java.io.*;
 import java.net.URL;
 import java.util.*;
 import java.lang.*;
 
 public class Kiirus {
 
-public static void annaKeskmine(int kiirus1, int kiirus2) {
-	System.out.printf("Kahe auto keskmine kiirus on: %.1f\n", ((float) kiirus1 + kiirus2) / 2);
-}
+    public static void annaKeskmine(double... tempo) {
+        double summa = 0;
+        for (double item : tempo) {
+            summa = summa + (1 / item);
+        }
+        System.out.printf("Sisestatud väärtuste keskmmine on: %.2f km/h\n", tempo.length / summa);
+    }
 
-public static void keskminineMassiivist(int... kiirused) {
-	int total = 0;
-	for(int aKiirused : kiirused) {
-		total = total + aKiirused;
-	}
-	System.out.printf("Sisestatud andmete keskmine on: %.1f\n", (float) total / kiirused.length);
-}
+    public static void keskminineMassiivist(int... kiirused) {
+        int total = 0;
+        for (int aKiirused : kiirused) {
+            total = total + aKiirused;
+        }
+        System.out.printf("Sisestatud andmete keskmine on: %.1f\n", (float) total / kiirused.length);
+    }
 
-public static void loeVeebist() throws IOException {
-	double vastus;
-	double total = 0;
-	URL url = new URL("http://www.tlu.ee/~jaagup/andmed/muu/pikkused.txt");
-	Scanner s = new Scanner(url.openStream());
-	String content = s.nextLine();
-	String[] array = content.split("\\s+");
-	for(String element:array) {
-		total = total + Integer.parseInt(element);
-	}
-	vastus = total/array.length;
-	System.out.println(vastus);
-}
+    public static String loeVeebist() throws IOException {
+        String vastus;
+        double total = 0;
+        URL url = new URL("http://www.tlu.ee/~jaagup/andmed/muu/pikkused.txt");
+        Scanner s = new Scanner(url.openStream());
+        String content = s.nextLine();
+        String[] array = content.split("\\s+");
 
-public static void main(String args[]) throws IOException {
-	annaKeskmine(70, 90);
-	keskminineMassiivist(60, 70, 90, 120);
-	loeVeebist();
+        for (String element : array) {
+            double helper = Integer.parseInt(element);
+            total += 1 / helper;
+        }
 
 
+        vastus = String.valueOf(array.length / total);
+        return vastus;
+    }
+
+    public static void loeFaili() throws IOException {
+
+        // FAILIST JA VEEBIST LUGEMINE
+        Scanner in = new Scanner(new FileReader("vastus.txt"));
+        String failiSisu = in.nextLine();
+        String veebiSisu = loeVeebist();
+
+        if (failiSisu.equals(veebiSisu)) {
+            System.out.println("Faili sisu on nagu peab.");
+
+        } else {
+            // KIRJUTAMINE
+            PrintWriter writer = new PrintWriter("vastus.txt", "UTF-8");
+            writer.println(loeVeebist());
+            writer.close();
+        }
 
 
+    }
 
-}
+
+    public static void main(String args[]) throws IOException {
+        annaKeskmine(30, 60, 60, 30);
+        keskminineMassiivist(60, 70, 90, 120);
+        loeFaili();
+
+    }
 }
