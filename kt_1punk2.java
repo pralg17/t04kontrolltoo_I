8efa312346bdc69eea1c991301441585cd62e10a
GittanKaus <GittanKaus@users.noreply.github.com>
2017-03-03 13:52:24 +0200

    Delete kt_1punk2.java

--- kt_1punk2.java
+++
@@ -1,7 +0,0 @@
-import java.util.*
-class kt_1punk2{
-	int[] nums={6,-1,-2,-3,0,1,2,3,4};
-	Arrays.sort(nums);
-	System.out.println("Minimum = " + nums[0]);
-	System.out.println("Maximum = " + nums[nums.length-1]);
-}
