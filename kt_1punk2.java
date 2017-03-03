155efe682b7b11f580c096e8ff9691d7737ebe6f
gittkaus <gittkaus@greeny.cs.tlu.ee>
2017-03-03 13:24:22 +0200

    KT esimene osa

---
+++ kt_1punk2.java
@@ -0,0 +1,7 @@
+import java.util.*
+class kt_1punk2{
+	int[] nums={6,-1,-2,-3,0,1,2,3,4};
+	Arrays.sort(nums);
+	System.out.println("Minimum = " + nums[0]);
+	System.out.println("Maximum = " + nums[nums.length-1]);
+}
