77230759a22a3c7ee96f8ec58809d3b5d14a375c
gittkaus <gittkaus@greeny.cs.tlu.ee>
2017-03-03 13:49:59 +0200

    KT esimene osa

--- kt_1punkt.java
+++ kt_1punkt.java
@@ -1,14 +1,22 @@
+import java.util.Arrays;
 class kt_1punkt{
 	public static void main(String args[]){
     int array[] = new int[]{10, 11, 88, 2, 12, 120};
- 
-		int max = getMax(array);
-		System.out.println("Maximum Value is: "+max);
- 
-		int min = getMin(array);
-		System.out.println("Minimum Value is: "+min);
 	
+	Arrays.sort(array);
+
+	System.out.println("The sorted int array is:");
+	for (int number : array) {
+	System.out.println("Number = " + number);
+		
 	}
+	int max = getMax(array);
+	System.out.println("Maximum Value is: "+max);
+ 
+	int min = getMin(array);
+	System.out.println("Minimum Value is: "+min);
+
+}
 
 	public static int getMax(int[] inputArray){ 
 		int maxValue = inputArray[0]; 
@@ -20,7 +28,7 @@ class kt_1punkt{
 		return maxValue; 
 	}
  
-	 public static int getMin(int[] inputArray){ 
+	public static int getMin(int[] inputArray){ 
 		int minValue = inputArray[0]; 
 		for(int i=1;i<inputArray.length;i++){ 
 		  if(inputArray[i] < minValue){ 
@@ -30,3 +38,4 @@ class kt_1punkt{
 		return minValue; 
 	} 
 }
+	
