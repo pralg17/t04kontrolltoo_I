3916ade79d090619f28ccf627aee4393b8000d82
johalaas <johalaas@greeny.cs.tlu.ee>
2017-05-17 12:51:42 +0300

    KT

--- keskmine.java
+++ keskmine.java
@@ -1,10 +1,10 @@
 public class keskmine{
-	public static void main(Strings[] args){
-		double numbrid=keskmine(1,1,1);
-		System.out.println(numbrid);
-	}
 	public static double keskmine(double arv1, double arv2, double arv3){
-		keskmine = (arv1/2)+arv2+(arv3/2);
+		double keskmine = 0;
+		keskmine = ((arv1/2)+arv2+(arv3/2))/2;
 		return keskmine;
+	}
+	public static void main(String[] arg){
+		System.out.println(keskmine(3, 4, 5));
 	}
 }
